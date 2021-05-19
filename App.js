5b7c9fea3cf2ba50de9067edc025d144c21ce139
Sahasra <73017579+sahasra09@users.noreply.github.com>
2021-05-19 19:39:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import SignUpLogin from './screens/WelcomeScreen'
 export default  class App extends React.Component{
   render(){
     return(
-      <WelcomeScreen/>
+      <SignUpLogin/>
     )
   }
 }
